2631f6f39403c255f74fdb71b5461ffd54e9dcc1
miguelprogrammer <miguel.silva@mackenzista.com.br>
2026-02-27 07:35:08 -0300

    :construction: endereço na memória e scanf

--- src/hello.go
+++ src/hello.go
@@ -3,8 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	nome := "MiguelProgrammer"
-	idade := 36
-	fmt.Println("Olá, mundo GO")
-	fmt.Println(nome, idade)
+	nome := "Miguel Silva"
+	versao := 1.1
+	fmt.Println("Olá, ", nome)
+
+	fmt.Println("Menu Sistema \n\n 1 - Iniciar Programa \n 2 - Exibir Logs \n 3 - Sair \n\n")
+
+	var comando int
+	fmt.Scanf("%d", &comando)
+	fmt.Println("Endereço na memória para a var comando: ", &comando)
+
+	fmt.Println("Opção selecionada: ", comando)
+
+	fmt.Println("Versão do programa", versao)
 }
